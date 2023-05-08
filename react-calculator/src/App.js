416a747c909ff6ae5d1ee560ffd1f26eafe98bfc
jerenteria <renteria.juan96@gmail.com>
2023-05-07 20:08:54 -0700

    restarted the project

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,9 +1,53 @@
+import React, { useReducer } from 'react';
 import './App.css';
+import DigitButton from "./components/DigitButton";
+
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+}
+
+function reducer(state, {type, payload}) {
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""} ${payload.digit}`,
+      }
+  }
+}
 
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, [])
+
   return (
-    <div className="App">
-      <h1>Hello!</h1>
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="previous-operand">{previousOperand}</div>
+  <div className="current-oprand">{currentOperand}</div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <DigitButton digit="/" dispatch={dispatch} />
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
