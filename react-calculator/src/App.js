56560dcb09604742019493578b8d2775f268a4cd
jerenteria <renteria.juan96@gmail.com>
2023-05-09 20:40:13 -0700

    cannot figure out bug for operants

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from 'react';
 import './App.css';
 import DigitButton from "./components/DigitButton";
+import OperationButton from './components/OperationButton';
 
 
 export const ACTIONS = {
@@ -11,40 +12,40 @@ export const ACTIONS = {
   EVALUATE: 'evaluate'
 }
 
-function reducer(state, {type, payload}) {
+function reducer(state, { type, payload }) {
   switch(type) {
     case ACTIONS.ADD_DIGIT:
       return {
         ...state,
-        currentOperand: `${state.currentOperand || ""} ${payload.digit}`,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`,
       }
   }
 }
 
 function App() {
-  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, [])
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
 
   return (
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">{previousOperand}</div>
-  <div className="current-oprand">{currentOperand}</div>
+  <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
-      <DigitButton digit="/" dispatch={dispatch} />
+      <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
       <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton digit="*" dispatch={dispatch} />
+      <OperationButton operation="*" dispatch={dispatch} />
       <DigitButton digit="4" dispatch={dispatch} />
       <DigitButton digit="5" dispatch={dispatch} />
       <DigitButton digit="6" dispatch={dispatch} />
-      <Operationbutton digit="+" dispatch={dispatch} />
+      <OperationButton operation="+" dispatch={dispatch} />
       <DigitButton digit="7" dispatch={dispatch} />
       <DigitButton digit="8" dispatch={dispatch} />
       <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton digit="-" dispatch={dispatch} />
+      <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
       <button className="span-two">=</button>
